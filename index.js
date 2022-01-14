726cabc2d9320562023c3e373b3dd76671f1e31e
karanja1victor <karanja1victor@gmail.com>
2022-01-14 11:44:27 +0300

    Create a prototype for scrapping Sportpesa and Betway for odds.

---
+++ index.js
@@ -0,0 +1,9 @@
+const { writeFile } = require('fs').promises;
+const Odds = require('./odds/odds.js');
+
+let janOdds = new Odds();
+
+setTimeout(async () => {
+    // await writeFile('eventsTest.txt', await janOdds.sportpesaOdds(), 'utf8');
+    console.log( await janOdds.betwayOdds()); 
+}, 1000);
